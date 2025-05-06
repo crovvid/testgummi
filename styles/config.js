39a65a6f92cd634f8a84085c255b0894aaf9658c
crovvid <57576856+crovvid@users.noreply.github.com>
2025-05-05 21:31:22 -0700

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1PGUw5pDs4zwqnjwXZoxc5F7HsMEg9rqwsTsEnkFUbrs";
 
 
 /* ------------------------------------------------------------------- */
